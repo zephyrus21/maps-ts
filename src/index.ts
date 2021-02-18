1d436c7f21b0a6d8482339ec2ba279738dbd4323
zephyrus21 <pandey.piyus@gmail.com>
2021-02-18 19:19:20 +0530

    integrating google maps

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import { User } from './User';
 import { Company } from './Company';
+
+// ! this will get the google map API and select the div in html with id of map and display the map there
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
